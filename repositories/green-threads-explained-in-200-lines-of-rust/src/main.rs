9e398a9f7b2e82a98d98025c8fe9462764de510a
ohke <kenta1988@gmail.com>
2022-03-12 13:28:55 +0900

    The stack

--- repositories/green-threads-explained-in-200-lines-of-rust/src/main.rs
+++ repositories/green-threads-explained-in-200-lines-of-rust/src/main.rs
@@ -1,15 +1,38 @@
 use core::arch::asm;
+use std::io::Write;
 
-const SSIZE: isize = 48;
+const SSIZE: isize = 1024;
+static mut S_PTR: *const u8 = 0 as *const u8;
 
 #[derive(Debug, Default)]
 #[repr(C)] // Cの構造体のメモリマップにする
 struct ThreadContext {
     rsp: u64, // stack pointer
+    r15: u64,
+    r14: u64,
+    r13: u64,
+    r12: u64,
+    rbx: u64,
+    rbp: u64,
+}
+
+fn print_stack(filename: &str) {
+    let mut f = std::fs::File::create(filename).unwrap();
+    unsafe {
+        for i in (0..SSIZE).rev() {
+            writeln!(
+                f,
+                "{:x} {:x}",
+                S_PTR.offset(i as isize) as usize,
+                *S_PTR.offset(i as isize)
+            );
+        }
+    }
 }
 
 fn hello() -> ! {
     println!("I LOVE WAKING UP ON A NEW STACK!");
+    print_stack("AFTER.txt");
     loop {}
 }
 
@@ -24,15 +47,19 @@ unsafe fn gt_switch(new: *const ThreadContext) {
 fn main() {
     let mut ctx = ThreadContext::default();
     let mut stack = vec![0_u8; SSIZE as usize];
+    let stack_ptr = stack.as_mut_ptr();
 
     unsafe {
         let stack_bottom = stack.as_mut_ptr().offset(SSIZE);
         // 16バイトアラインメント (stack_bottomの先頭が16バイトで割り切れない場合は切り捨てる)
         let sb_aligned = (stack_bottom as usize & !15) as *mut u8;
+        S_PTR = sb_aligned;
         // hello関数のアドレスで書き換える
-        std::ptr::write(sb_aligned.offset(-16) as *mut u64, hello as u64);
+        std::ptr::write(stack_ptr.offset(SSIZE - 16) as *mut u64, hello as u64);
+        print_stack("BEFORE.txt");
         // スタックポインタを書き換えたアドレスをセット
-        ctx.rsp = sb_aligned.offset(-16) as u64;
+        ctx.rsp = stack_ptr.offset(SSIZE - 16) as u64;
+
         gt_switch(&mut ctx);
     }
 }
