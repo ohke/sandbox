37b4ca607d23d1f7cadc6e4b1371f8b78178292b
ohke <kenta1988@gmail.com>
2022-03-17 22:32:06 +0900

    actix-web websocket-chat (main)

---
+++ repositories/actix-example/websocket-chat/src/main.rs
@@ -0,0 +1,71 @@
+use actix::*;
+use actix_files::{Files, NamedFile};
+use actix_web::{
+    middleware::Logger,
+    web::{self, service},
+    App, Error, HttpRequest, HttpResponse, HttpServer, Responder,
+};
+use actix_web_actors::ws;
+use std::{
+    sync::{
+        atomic::{AtomicUsize, Ordering},
+        Arc,
+    },
+    time::Instant,
+};
+
+// mod server;
+// mod session;
+
+async fn index() -> impl Responder {
+    NamedFile::open_async("./static/index.html").await.unwrap()
+}
+
+// async fn chat_route(
+//     req: HttpRequest,
+//     stream: web::Payload,
+//     srv: web::Data<Addr<server::ChatServer>>,
+// ) -> Result<HttpResponse, Error> {
+//     ws::start(
+//         session::WsChatSession {
+//             id: 0,
+//             hb: Instant::now(),
+//             room: "Main".to_owned(),
+//             name: None,
+//             addr: srv.get_ref().clone(),
+//         },
+//         &req,
+//         stream,
+//     )
+// }
+
+async fn get_count(count: web::Data<AtomicUsize>) -> impl Responder {
+    let current_count = count.load(Ordering::SeqCst);
+    format!("Visitors: {}", current_count)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+
+    let app_state = Arc::new(AtomicUsize::new(0));
+
+    // let server = server::ChatServer::new(app_state.clone()).start();
+
+    log::info!("starting HTTP server at http://localhost:8080");
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(web::Data::from(app_state.clone()))
+            // .app_data(web::Data::new(server.clone()))
+            .service(web::resource("/").to(index))
+            .route("/count", web::get().to(get_count))
+            // .route("/ws", web::get().to(chat_route))
+            .service(Files::new("/static", "./static"))
+            .wrap(Logger::default())
+    })
+    .workers(2)
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
