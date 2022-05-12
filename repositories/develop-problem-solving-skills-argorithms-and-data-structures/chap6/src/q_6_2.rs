67dfea0ca82f6e560e14fe395295d392552c8598
ohke <kenta1988@gmail.com>
2022-05-12 09:26:12 +0900

    chap6 q_6_2(fix)

--- repositories/develop-problem-solving-skills-argorithms-and-data-structures/chap6/src/q_6_2.rs
+++ repositories/develop-problem-solving-skills-argorithms-and-data-structures/chap6/src/q_6_2.rs
@@ -1,17 +1,17 @@
-fn upper_bound(a: &Vec<u64>, v: u64) -> Option<usize> {
+fn lower_bound(a: &Vec<u64>, v: u64) -> Option<usize> {
     // v < a[i] を満たす最小の i を返す
     let mut left = 0;
     let mut right = a.len() - 1;
 
-    if a[right] <= v {
+    if a[right] < v {
         return None;
-    } else if v < a[left] {
+    } else if v <= a[left] {
         return Some(0);
     }
 
     while left <= right {
         let mid = left + (right - left) / 2;
-        if a[mid] <= v {
+        if a[mid] < v {
             left = mid + 1;
         } else {
             right = mid - 1;
@@ -34,12 +34,12 @@ fn solve(a: Vec<u64>, b: Vec<u64>, c: Vec<u64>) -> u64 {
     c.sort();
 
     for j in 0..n {
-        let n_a = match upper_bound(&a, b[j]) {
+        let n_a = match lower_bound(&a, b[j]) {
             Some(i) => i,
             None => n,
         };
 
-        let n_c = match upper_bound(&c, b[j]) {
+        let n_c = match lower_bound(&c, b[j] + 1) {
             Some(k) => n - k,
             None => 0,
         };
@@ -74,5 +74,25 @@ mod tests {
             ),
             87
         );
+    }
+
+    #[test]
+    fn test_lower_bound_1() {
+        assert_eq!(lower_bound(&vec![1, 1, 2, 3, 3, 4], 3), Some(3));
+    }
+
+    #[test]
+    fn test_lower_bound_2() {
+        assert_eq!(lower_bound(&vec![1, 1, 2, 3, 3, 4], 1), Some(0));
+    }
+
+    #[test]
+    fn test_lower_bound_3() {
+        assert_eq!(lower_bound(&vec![1, 1, 2, 3, 3, 4], 4), Some(5));
+    }
+
+    #[test]
+    fn test_lower_bound_4() {
+        assert_eq!(lower_bound(&vec![1, 1, 2, 3, 3, 4], 5), None);
     }
 }
