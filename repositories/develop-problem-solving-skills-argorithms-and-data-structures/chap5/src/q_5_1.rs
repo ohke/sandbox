260a399c78875b8c04bc0041c4cd0ac3238740b7
ohke <kenta1988@gmail.com>
2022-06-24 09:31:37 +0900

    typical90 001

--- repositories/develop-problem-solving-skills-argorithms-and-data-structures/chap5/src/q_5_1.rs
+++ repositories/develop-problem-solving-skills-argorithms-and-data-structures/chap5/src/q_5_1.rs
@@ -1,32 +1,35 @@
 fn solve(n: usize, a: Vec<u64>, b: Vec<u64>, c: Vec<u64>) -> u64 {
-    let mut dp = vec![vec![0; 3]; 100000];
+    let mut dp = vec![vec![0; 3]; 2];
 
     dp[0][0] = a[0];
     dp[0][1] = b[0];
     dp[0][2] = c[0];
 
     for i in 1..n {
+        let i_1 = i % 2;
+        let i_0 = (i - 1) % 2;
+
         for j in 0..3 {
-            dp[i][j] = match j {
+            dp[i_1][j] = match j {
                 0 => {
-                    if dp[i - 1][1] < dp[i - 1][2] {
-                        dp[i - 1][2] + a[i]
+                    if dp[i_0][1] < dp[i_0][2] {
+                        dp[i_0][2] + a[i]
                     } else {
-                        dp[i - 1][1] + a[i]
+                        dp[i_0][1] + a[i]
                     }
                 }
                 1 => {
-                    if dp[i - 1][0] < dp[i - 1][2] {
-                        dp[i - 1][2] + b[i]
+                    if dp[i_0][0] < dp[i_0][2] {
+                        dp[i_0][2] + b[i]
                     } else {
-                        dp[i - 1][0] + b[i]
+                        dp[i_0][0] + b[i]
                     }
                 }
                 2 => {
-                    if dp[i - 1][0] < dp[i - 1][1] {
-                        dp[i - 1][1] + c[i]
+                    if dp[i_0][0] < dp[i_0][1] {
+                        dp[i_0][1] + c[i]
                     } else {
-                        dp[i - 1][0] + c[i]
+                        dp[i_0][0] + c[i]
                     }
                 }
                 _ => panic!(),
@@ -34,7 +37,7 @@ fn solve(n: usize, a: Vec<u64>, b: Vec<u64>, c: Vec<u64>) -> u64 {
         }
     }
 
-    *dp[n - 1].iter().max().unwrap()
+    *dp[(n - 1) % 2].iter().max().unwrap()
 }
 
 #[cfg(test)]
--- repositories/rust-atcoder/typical90/src/bin/001.rs
+++ repositories/rust-atcoder/typical90/src/bin/001.rs
@@ -1,3 +1,57 @@
 fn main() {
-    todo!();
+    proconio::input! {
+        n: usize,
+        l: usize,
+    }
+
+    proconio::input! {
+        k: usize,
+    }
+
+    proconio::input! {
+        a: [usize; n],
+    }
+
+    let mut v = Vec::new();
+    v.push(a[0]);
+    for i in 1..a.len() {
+        v.push(a[i] - a[i-1]);
+    }
+    v.push(l - a[a.len() - 1]);
+
+    let mut s = 0;
+
+    let mut left = 0;
+    let mut right = l;
+    
+    while left <= right {
+        let mid = left + (right - left) / 2;
+
+        let mut n_blocks = 0;
+        let mut acc = 0;
+        let mut score = std::usize::MAX;
+        
+        for v_i in v.iter() {
+            acc += v_i;
+            if acc >= mid {
+                score = score.min(acc);
+                acc = 0;
+                n_blocks += 1;
+            }
+        }
+
+        if n_blocks == k+1 {
+            s = s.max(score);
+        }
+
+        if left == right {
+            break;
+        } else if n_blocks >= k+1 {
+            left = mid + 1;   
+        } else {
+            right = mid;
+        }
+    }
+
+    println!("{}", s);
 }
