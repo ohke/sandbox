9948c9a6af95970d413c2fbfa2c193f014cc1a67
ohke <kenta1988@gmail.com>
2022-06-25 14:21:33 +0900

    typical90 002

--- repositories/rust-atcoder/typical90/src/bin/002.rs
+++ repositories/rust-atcoder/typical90/src/bin/002.rs
@@ -1,3 +1,32 @@
 fn main() {
-    todo!();
+    proconio::input! {
+        n: u32,
+    }
+
+    for i in 0..2_u32.pow(n) {
+        let mut s = Vec::new();
+        let mut r = 0;
+        let mut ok = true;
+
+        for j in 0..n {
+            let mask = 2_u32.pow(n - j - 1);
+            if i & mask == 0 {
+                s.push('(');
+                r += 1;
+            } else {
+                s.push(')');
+                r -= 1;
+            }
+
+            if r < 0 {
+                ok = false;
+                break;
+            }
+        }
+
+        if ok && r == 0 {
+            let s: String = s.into_iter().collect();
+            println!("{}", &s);
+        }
+    }
 }
