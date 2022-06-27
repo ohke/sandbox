1e1d17b1b6c56cdb93ed650a859a073ce6fe9a14
ohke <kenta1988@gmail.com>
2022-06-27 10:18:42 +0900

    typical90 007

--- repositories/rust-atcoder/typical90/src/bin/007.rs
+++ repositories/rust-atcoder/typical90/src/bin/007.rs
@@ -1,3 +1,41 @@
+use superslice::Ext;
+
 fn main() {
-    todo!();
+    proconio::input! {
+        n: usize,
+    }
+
+    proconio::input! {
+        mut a: [i64; n],
+    }
+
+    proconio::input! {
+        q: usize,
+    }
+
+    let mut b = Vec::new();
+    for _ in 0..q {
+        proconio::input! {
+            b_i: i64,
+        }
+
+        b.push(b_i);
+    }
+
+    a.sort();
+
+    for b_i in b.iter() {
+        let index = a.upper_bound(b_i);
+
+        let ret = if index == 0 || index == a.len() - 1 {
+            (a[index] - b_i).abs()
+        } else if index == a.len() {
+            (a[index - 1] - b_i).abs()
+        } else {
+            (a[index] - b_i).abs().min((a[index - 1] - b_i).abs())
+        };
+
+        println!("{}", ret);
+    }
+    
 }
