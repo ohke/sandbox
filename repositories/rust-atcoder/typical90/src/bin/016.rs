20ad6fca09cf7c07b25f9b92b206093ef974296f
ohke <kenta1988@gmail.com>
2022-06-29 09:28:54 +0900

    typical90 016

--- repositories/rust-atcoder/typical90/src/bin/016.rs
+++ repositories/rust-atcoder/typical90/src/bin/016.rs
@@ -1,3 +1,32 @@
 fn main() {
-    todo!();
+    proconio::input! {
+        n: i64,
+    }
+
+    proconio::input! {
+        a: i64,
+        b: i64,
+        c: i64,
+    }
+
+    let mut ret = std::i64::MAX;
+    for x in 0..10000 {
+        let r = n - a*x;
+        if r < 0 {
+            break;
+        }
+
+        for y in 0..10000 {
+            let r = r - b*y;
+            if r < 0 {
+                break;
+            }
+
+            if r % c == 0 {
+                ret = ret.min(x + y + r / c);
+            }
+        }
+    }
+
+    println!("{}", ret);
 }
