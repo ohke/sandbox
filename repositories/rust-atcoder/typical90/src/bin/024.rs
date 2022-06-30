21a5efb2e256ccc5cdac53d2a9b0c0322b07165e
ohke <kenta1988@gmail.com>
2022-07-01 08:42:04 +0900

    typical90 024

--- repositories/rust-atcoder/typical90/src/bin/024.rs
+++ repositories/rust-atcoder/typical90/src/bin/024.rs
@@ -1,3 +1,22 @@
 fn main() {
-    todo!();
+    proconio::input! {
+        n: usize,
+        k: i64,
+    }
+
+    proconio::input! {
+        a: [i64; n],
+        b: [i64; n],
+    }
+
+    let mut diff = 0;
+    for i in 0..n {
+        diff += (a[i] - b[i]).abs();
+    }
+
+    if k >= diff && (k - diff) % 2 == 0 {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
 }
