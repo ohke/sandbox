a3b054f51ace609397f1a248ec892806c14f6c49
ohke <kenta1988@gmail.com>
2022-07-01 08:14:35 +0900

    typical90 020

--- repositories/rust-atcoder/typical90/src/bin/020.rs
+++ repositories/rust-atcoder/typical90/src/bin/020.rs
@@ -1,3 +1,13 @@
 fn main() {
-    todo!();
+    proconio::input! {
+        a: usize,
+        b: u32,
+        c: usize,
+    }
+
+    if a < c.pow(b) {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
 }
