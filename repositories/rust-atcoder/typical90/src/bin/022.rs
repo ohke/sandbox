34f96de3bc0a139fdd90451e8741a40e7ea9e765
ohke <kenta1988@gmail.com>
2022-07-01 08:30:27 +0900

    typical90 022

--- repositories/rust-atcoder/typical90/src/bin/022.rs
+++ repositories/rust-atcoder/typical90/src/bin/022.rs
@@ -1,3 +1,29 @@
+fn gcd(mut x: usize, mut y: usize) -> usize {
+    loop {
+        if x >= y {
+            x = x % y;
+            if x == 0 {
+                return y;
+            }
+        } else {
+            y = y % x;
+            if y == 0 {
+                return x;
+            }
+        }
+    }
+}
+
 fn main() {
-    todo!();
+    proconio::input! {
+        a: usize,
+        b: usize,
+        c: usize,
+    }
+
+    let r = gcd(a, gcd(b, c));
+
+    let ret = (a/r - 1) + (b/r - 1) + (c/r - 1);
+
+    println!("{}", ret);
 }
