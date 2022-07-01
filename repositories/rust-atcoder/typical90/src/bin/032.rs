f100c6261049cf794162e08fbcd4d4216bca14f4
ohke <kenta1988@gmail.com>
2022-07-01 09:22:04 +0900

    typical90 032

--- repositories/rust-atcoder/typical90/src/bin/032.rs
+++ repositories/rust-atcoder/typical90/src/bin/032.rs
@@ -1,3 +1,50 @@
+use itertools::Itertools;
+
 fn main() {
-    todo!();
+    proconio::input! {
+        n: usize,
+    }
+
+    proconio::input! {
+        a: [[usize; n]; n],
+    }
+
+    proconio::input! {
+        m: usize,
+    }
+
+    let mut ng = vec![vec![false; n + 1]; n + 1];
+    for _ in 0..m {
+        proconio::input! {
+            x_i: usize,
+            y_i: usize,
+        }
+
+        ng[x_i][y_i] = true;
+        ng[y_i][x_i] = true;
+    }
+
+    let mut min = std::usize::MAX;
+    for npr in (1..(n + 1)).permutations(n) {
+        let mut t = 0;
+        let mut all_ok = true;
+
+        for (j, i) in npr.iter().enumerate() {
+            if j > 0 && ng[npr[j - 1]][*i] {
+                all_ok = false;
+                break;
+            }
+            t += a[*i - 1][j];
+        }
+
+        if all_ok {
+            min = min.min(t);
+        }
+    }
+
+    if min < std::usize::MAX {
+        println!("{}", min);
+    } else {
+        println!("-1");
+    }
 }
