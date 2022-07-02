a2615f08f1c01aa73dff2bb3232b8e330f07b864
ohke <kenta1988@gmail.com>
2022-07-02 11:07:32 +0900

    typical90 038

--- repositories/rust-atcoder/typical90/src/bin/038.rs
+++ repositories/rust-atcoder/typical90/src/bin/038.rs
@@ -1,3 +1,31 @@
 fn main() {
-    todo!();
+    proconio::input! {
+        a: u64,
+        b: u64,
+    }
+
+    let gcd;
+    let mut x = a;
+    let mut y = b;
+    loop {
+        if x < y {
+            y = y % x;
+            if y == 0 {
+                gcd = x;
+                break;
+            }
+        } else {
+            x = x % y;
+            if x == 0 {
+                gcd = y;
+                break;
+            }
+        }
+    }
+
+    if b / gcd <= 1000000000000000000 / a {
+        println!("{}", a * b / gcd);
+    } else {
+        println!("Large");
+    }
 }
