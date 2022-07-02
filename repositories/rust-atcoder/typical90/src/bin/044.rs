df4f24b4ffcc1813db26315c52ed0aba3ed295e1
ohke <kenta1988@gmail.com>
2022-07-02 11:27:34 +0900

    typical90 044

--- repositories/rust-atcoder/typical90/src/bin/044.rs
+++ repositories/rust-atcoder/typical90/src/bin/044.rs
@@ -1,3 +1,34 @@
 fn main() {
-    todo!();
+    proconio::input! {
+        n: usize,
+        q: usize,
+    }
+
+    proconio::input! {
+        mut a: [usize; n],
+    }
+
+    let mut base = 0;
+
+    for _ in 0..q {
+        proconio::input! {
+            t: usize,
+            mut x: usize,
+            mut y: usize,
+        }
+
+        if x > 0 {
+            x = ((x - 1) + base) % n;
+        }
+        if y > 0 {
+            y = ((y - 1) + base) % n;
+        }
+
+        match t {
+            1 => a.swap(x, y),
+            2 => base = if base > 0 { (base - 1) % n } else { n - 1 },
+            3 => println!("{}", a[x]),
+            _ => panic!(),
+        }
+    }
 }
