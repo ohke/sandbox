6fe549a99bf10e1011779471eb21f5571e138907
ohke <kenta1988@gmail.com>
2022-07-03 08:44:17 +0900

    typical90 048

--- repositories/rust-atcoder/typical90/src/bin/048.rs
+++ repositories/rust-atcoder/typical90/src/bin/048.rs
@@ -1,3 +1,30 @@
+use std::collections::BinaryHeap;
+
 fn main() {
-    todo!();
+    proconio::input! {
+        n: usize,
+        k: usize,
+    }
+
+    let mut heap = BinaryHeap::new();
+    for _ in 0..n {
+        proconio::input! {
+            a_i: usize,
+            b_i: usize,
+        }
+
+        heap.push((b_i, a_i - b_i));
+    }
+
+    let mut ret = 0;
+    for _ in 0..k {
+        let (b, a) = heap.pop().unwrap();
+
+        ret += b;
+        if a != 0 {
+            heap.push((a, 0));
+        }
+    }
+
+    println!("{}", ret);
 }
