0737ed3fd09613dfe1a6c666f67ab1e82657dd77
ohke <kenta1988@gmail.com>
2022-07-04 08:32:59 +0900

    typical90 052

--- repositories/rust-atcoder/typical90/src/bin/052.rs
+++ repositories/rust-atcoder/typical90/src/bin/052.rs
@@ -1,3 +1,15 @@
 fn main() {
-    todo!();
+    proconio::input! {
+        n: usize,
+        a: [[usize; 6]; n],
+    }
+
+    let mut ret = 1;
+    for i in 0..n {
+        let p: usize = a[i].iter().sum();
+        ret *= p;
+        ret %= 1000000007;
+    }
+
+    println!("{}", ret);
 }
