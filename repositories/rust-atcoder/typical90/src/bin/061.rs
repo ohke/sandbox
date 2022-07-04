64b5b1836069b062c57c8a08295cc22615ae4f1b
ohke <kenta1988@gmail.com>
2022-07-04 09:09:14 +0900

    typical90 061

--- repositories/rust-atcoder/typical90/src/bin/061.rs
+++ repositories/rust-atcoder/typical90/src/bin/061.rs
@@ -1,3 +1,20 @@
 fn main() {
-    todo!();
+    proconio::input! {
+        q: usize,
+    }
+
+    let mut x = Vec::new();
+    for _ in 0..q {
+        proconio::input! {
+            t_i: usize,
+            x_i: usize,
+        }
+
+        match t_i {
+            1 => x.insert(0, x_i),
+            2 => x.push(x_i),
+            3 => println!("{}", x[x_i - 1]),
+            _ => panic!(),
+        }
+    }
 }
