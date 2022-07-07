35349dd7a6dd1e1a7c646d226be7a499f02e69d6
ohke <kenta1988@gmail.com>
2022-07-08 08:14:52 +0900

    typical90 076

--- repositories/rust-atcoder/typical90/src/bin/076.rs
+++ repositories/rust-atcoder/typical90/src/bin/076.rs
@@ -1,3 +1,29 @@
 fn main() {
-    todo!();
+    proconio::input! {
+        n: usize,
+        a: [usize; n],
+    }
+
+    let sum: usize = a.iter().sum();
+
+    let mut i = 0;
+    let mut j = 1;
+    // [a_i, a_j) の合計
+    let mut current = a[i];
+    loop {
+        if current * 10 < sum {
+            current += a[j];
+            j = (j + 1) % n;
+        } else if current * 10 > sum {
+            current -= a[i];
+            i += 1;
+            if i == n {
+                println!("No");
+                break;
+            }
+        } else {
+            println!("Yes");
+            break;
+        }
+    }
 }
