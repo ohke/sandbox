1445c071c1f64879490487bc1975a97791588646
ohke <kenta1988@gmail.com>
2022-07-07 09:11:27 +0900

    typical90 075

--- repositories/rust-atcoder/typical90/src/bin/075.rs
+++ repositories/rust-atcoder/typical90/src/bin/075.rs
@@ -1,3 +1,25 @@
 fn main() {
-    todo!();
+    proconio::input! {
+        mut n: usize,
+    }
+
+    let mut n_primes = 0;
+    for i in 2..((n as f64).sqrt().ceil() as usize + 1) {
+        loop {
+            if n % i == 0 {
+                n_primes += 1;
+                n /= i;
+            } else {
+                break;
+            }
+        }
+    }
+
+    let ret = if n_primes == 0 {
+        0
+    } else {
+        f64::log2(n_primes as f64).ceil() as usize
+    };
+
+    println!("{}", ret);
 }
