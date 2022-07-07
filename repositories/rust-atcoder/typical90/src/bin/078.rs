f5e88336c4179b5911caed7657a095cd8eb3b695
ohke <kenta1988@gmail.com>
2022-07-08 08:27:01 +0900

    typical90 078

--- repositories/rust-atcoder/typical90/src/bin/078.rs
+++ repositories/rust-atcoder/typical90/src/bin/078.rs
@@ -1,3 +1,26 @@
 fn main() {
-    todo!();
+    proconio::input! {
+        n: usize,
+        m: usize,
+    }
+
+    let mut v = vec![0; n];
+
+    for _ in 0..m {
+        proconio::input! {
+            a_i: usize,
+            b_i: usize,
+        }
+
+        if a_i < b_i {
+            v[b_i - 1] += 1;
+        } else if a_i > b_i {
+            v[a_i - 1] += 1;
+        } else {
+            panic!();
+        }
+    }
+
+    let ret = v.iter().filter(|v_i| **v_i == 1).count();
+    println!("{}", ret);
 }
