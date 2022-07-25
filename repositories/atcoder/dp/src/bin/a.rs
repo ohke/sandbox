aedf108c6dd6214f130a921a422c15238895d0ba
ohke <kenta1988@gmail.com>
2022-07-26 07:03:37 +0900

    atcoder dp a,b

---
+++ repositories/atcoder/dp/src/bin/a.rs
@@ -0,0 +1,23 @@
+use proconio::input;
+
+fn main() {
+    input! {
+        n: usize,
+        h: [i64; n],
+    }
+
+    let mut dp = vec![std::i64::MAX; n];
+
+    // 初期値
+    dp[0] = 0;
+    dp[1] = (h[1] - h[0]).abs();
+
+    // 更新
+    for i in 2..n {
+        dp[i] = ((h[i] - h[i-1]).abs() + dp[i-1]).min((h[i] - h[i-2]).abs() + dp[i-2]);
+    }
+
+    let result = dp[n-1];
+    
+    println!("{}", result);
+}
---
+++ repositories/atcoder/dp/src/bin/b.rs
@@ -0,0 +1,28 @@
+use proconio::input;
+
+fn main() {
+    input! {
+        n: usize,
+        k: usize,
+        h: [i64; n],
+    }
+
+    let mut dp = vec![std::i64::MAX; n];
+
+    // 初期化
+    dp[0] = 0;
+
+    // 更新
+    for i in 0..(n-1) {
+        for j in 1..(k+1) {
+            if i + j >= n {
+                break;
+            }
+
+            dp[i + j] = dp[i + j].min(dp[i] + (h[i + j] - h[i]).abs());
+        }
+    }
+
+    let result = dp[n-1];
+    println!("{}", result);
+}
